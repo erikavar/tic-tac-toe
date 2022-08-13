d4853d5f044cbdfc74ee745880ec4ae6f0ae8569
Erika <erikavarghese@gmail.com>
2022-08-12 22:57:11 -0400

    added logic so x appears when square is clicked

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-// board with dummy content just to visualize
+// board
 const Gameboard = (() => {
-    let gameBoard = ["X","X","X","X","X","X","X","X","X"];
+    let gameBoard = ["","","","","","","","",""];
+   
+
+    const markSpot = document.querySelectorAll(".spot");
+    markSpot.forEach((div) => {
+        div.addEventListener("click", () => {
+            Gameboard.forEach((item, index) => {
+                /*if (div.id === Gameboard[index] === 1) {
+                    Gameboard[1] = "X";
+                }*/
+                console.log(item[index]);
+            });
+            div.textContent = "X";
+            console.log(gameBoard);
+            console.log(div.id);
+        });;
+    });
+
     return gameBoard;
+
 })();
 
 // populating the board using content from above
-// probably going to have to change this, the divs need to already exist so that they can be clicked
 const Game = (() => {
     let board = document.getElementById("boardContainer");
     Gameboard.forEach((item, index) => {
-        const div = document.createElement("div");
+        /*const div = document.createElement("div");
         div.setAttribute("class", "square");
         div.textContent = item;
-        board.appendChild(div);
+        board.appendChild(div);*/
     });
 })();
 
 // factory function for players
-const Player = (name, score) => {
-    return { name, score }
+const Player = (name, symbol, score) => {
+    return { name, symbol, score }
 };
